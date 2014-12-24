b495a8b4e8f78a504953750ec1835caa3164ed98
Sebastian Kloska <oncaphillis@snafu.de>
2014-12-25 00:58:40 +0100

    Transforming test app "main.cpp" to "baconbank"
    A app to calculate "bacon index" numbers

--- main.cpp
+++ main.cpp
@@ -7,6 +7,53 @@
 #include <tmdbpp/api.h>
 #include <tmdbpp/search.h>
 
+
+void ScanTmdb(int id,std::map<int,std::shared_ptr<tmdbpp::MediaCredits> > & movies,std::set<int> & persons,
+              time_t t0=0, int n=0,std::ostream & os=std::cerr) {
+    if(t0==0)
+        t0 = time(&t0);
+
+    if(persons.find(id)!=persons.end()) {
+        return;
+    }
+
+    time_t t1 = time(&t1);
+
+    std::cerr << (float) n / (float)(t1-t0) << std::endl;
+
+    tmdbpp::Api &api(tmdbpp::Api::instance());
+    tmdbpp::Person p=api.get().person(id);
+
+    os << "p:" << p.id() << ":" << p.name() << std::endl;
+
+    persons.insert(id);
+
+    tmdbpp::PersonalCredits mc = api.search().person().movie_credits(id);
+    n++;
+    for(auto m : mc.as_cast()) {
+        std::map<int,std::shared_ptr<tmdbpp::MediaCredits> >::const_iterator it;
+
+        tmdbpp::MediaCredits *mcp;
+
+        if((it=movies.find(m.media_id()))!=movies.end()) {
+            mcp = it->second.get();
+        } else {
+            mcp = (movies[m.media_id()] =
+            std::shared_ptr<tmdbpp::MediaCredits>(
+                new tmdbpp::MediaCredits(api.search().movie().credits(m.media_id())))).get();
+            n++;
+            os << "m:" << m.media_id() << ":" << m.title() << std::endl;
+            for( auto c : mcp->as_cast() ) {
+                os << "r:" << m.media_id() << ":" << c.id() << std::endl;
+            }
+        }
+
+        for(auto p : mcp->as_cast()) {
+            ScanTmdb(p.id(),movies,persons,t0,n,os);
+        }
+    }
+}
+
 int main(int, char **)
 {
     try
@@ -14,22 +61,29 @@ int main(int, char **)
 
         tmdbpp::Api &api(tmdbpp::Api::instance());
 
-        std::list<tmdbpp::Genre> gl = api.get().movie().genres();
-
-        int i=0;
-
-        for(auto g : gl) {
-            std::cerr << "@" << ++i << " " << g.id() << " " << g.name() << std::endl;
-        }
-        ::exit(1);
-
         int p=1;
+
+        std::list<tmdbpp::PersonSummary> l;
+
         while(true) {
             tmdbpp::Persons persons = api.search().person("kevin bacon",p++);
             if(persons.empty())
                 break;
 
-            std::cerr << persons.page() << "/" << persons.total_pages() << "@" << persons.total_results() << std::endl;
+            std::copy(persons.list().begin(),persons.list().end(),
+                      std::back_inserter(l));
+        }
+
+        std::map<int,std::shared_ptr<tmdbpp::MediaCredits> > movies;
+        std::set<int> persons;
+
+        if(l.size()==1)
+            ScanTmdb(l.front().id(),movies,persons);
+
+
+
+ #if 0
+        std::cerr << persons.page() << "/" << persons.total_pages() << "@" << persons.total_results() << std::endl;
             for(const auto & p : persons.list()) {
                 std::cerr << p.name() << std::endl;
                 auto pp = api.get().person(p.id());
@@ -49,6 +103,7 @@ int main(int, char **)
                 }
              }
         }
+#endif
         return 0;
     }
     catch( std::exception &e )
